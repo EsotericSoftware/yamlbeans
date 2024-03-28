6dd2680769e39bda2ad92dfdbd531642ae84455d
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2024-03-28 00:32:14 -0300

    refactor: replace conditional logic with polymorphism in YAML document reader
    - Implemented polymorphism to handle different YAML element types, enhancing code maintainability and extensibility.
    - Removed switch statements from readValue and readDocument methods, delegating to specific reader classes.
    - Introduced YamlElementReader interface and concrete implementations for each YAML element type.

--- src/com/esotericsoftware/yamlbeans/document/YamlDocumentReader.java
+++ src/com/esotericsoftware/yamlbeans/document/YamlDocumentReader.java
@@ -8,19 +8,14 @@ import java.util.Iterator;
 
 import com.esotericsoftware.yamlbeans.Version;
 import com.esotericsoftware.yamlbeans.YamlException;
-import com.esotericsoftware.yamlbeans.parser.AliasEvent;
-import com.esotericsoftware.yamlbeans.parser.Event;
-import com.esotericsoftware.yamlbeans.parser.MappingStartEvent;
-import com.esotericsoftware.yamlbeans.parser.Parser;
+import com.esotericsoftware.yamlbeans.parser.*;
 import com.esotericsoftware.yamlbeans.parser.Parser.ParserException;
-import com.esotericsoftware.yamlbeans.parser.ScalarEvent;
-import com.esotericsoftware.yamlbeans.parser.SequenceStartEvent;
 import com.esotericsoftware.yamlbeans.tokenizer.Tokenizer.TokenizerException;
 
 public class YamlDocumentReader {
 
 	Parser parser;
-	
+
 	public YamlDocumentReader(String yaml) {
 		this(new StringReader(yaml));
 	}
@@ -48,7 +43,7 @@ public class YamlDocumentReader {
 		try {
 			while (true) {
 				Event event = parser.peekNextEvent();
-				if (event == null) 
+				if (event == null)
 					return null;
 				switch (event.type) {
 				case STREAM_START:
@@ -61,7 +56,7 @@ public class YamlDocumentReader {
 					parser.getNextEvent(); // consume it
 					return (T)readDocument();
 				default:
-					throw new IllegalStateException();	
+					throw new IllegalStateException();
 				}
 			}
 		} catch (ParserException ex) {
@@ -96,24 +91,9 @@ public class YamlDocumentReader {
 	}
 
 	private YamlElement readDocument() {
-		YamlElement yamlElement = null;
 		Event event = parser.peekNextEvent();
-		switch (event.type) {
-		case SCALAR:
-			yamlElement = readScalar();
-			break;
-		case ALIAS:
-			yamlElement = readAlias();
-			break;
-		case MAPPING_START:
-			yamlElement = readMapping();
-			break;
-		case SEQUENCE_START:
-			yamlElement = readSequence();
-			break;
-		default:
-			throw new IllegalStateException();
-		}
+		YamlElementReader reader = getReaderForEventType(event.type);
+		YamlElement yamlElement = reader.read(parser);
 		parser.getNextEvent(); // consume it(DOCUMENT_END)
 		return yamlElement;
 	}
@@ -140,52 +120,105 @@ public class YamlDocumentReader {
 				YamlEntry entry = readEntry();
 				mapping.addEntry(entry);
 			}
-		}		
+		}
 	}
 
 	private YamlEntry readEntry() {
-		YamlScalar scalar = readScalar();
+		YamlScalar scalar = readScalarUsingReader();
 		YamlElement value = readValue();
 		return new YamlEntry(scalar, value);
 	}
 
+	private YamlScalar readScalarUsingReader() {
+		YamlElement element = new ScalarReader().read(parser);
+		if (!(element instanceof YamlScalar)) {
+			throw new IllegalStateException("Expected a YamlScalar but got " + element.getClass().getName());
+		}
+		return (YamlScalar) element;
+	}
+
 	private YamlElement readValue() {
 		Event event = parser.peekNextEvent();
-		switch(event.type) {
-			case SCALAR:
-				return readScalar();
-			case ALIAS:
-				return readAlias();
-			case MAPPING_START:
-				return readMapping();
-			case SEQUENCE_START:
-				return readSequence();
-			default:
+		YamlElementReader reader = getReaderForEventType(event.type);
+		return reader.read(parser);
+	}
+
+    private interface YamlElementReader {
+        YamlElement read(Parser parser);
+    }
+
+    private YamlElementReader getReaderForEventType(EventType eventType) {
+        switch (eventType) {
+            case SCALAR:
+                return new ScalarReader();
+            case ALIAS:
+                return new AliasReader();
+            case MAPPING_START:
+                return new MappingReader();
+            case SEQUENCE_START:
+                return new SequenceReader();
+            default:
+                throw new IllegalStateException();
+        }
+    }
+
+    private class ScalarReader implements YamlElementReader {
+        @Override
+        public YamlElement read(Parser parser) {
+            Event event = parser.getNextEvent();
+            if(event.type!= SCALAR)
+                throw new IllegalStateException();
+            ScalarEvent scalar = (ScalarEvent)event;
+            YamlScalar element = new YamlScalar();
+            element.setTag(scalar.tag);
+            element.setAnchor(scalar.anchor);
+            element.setValue(scalar.value);
+            return element;
+        }
+    }
+
+    private class SequenceReader implements YamlElementReader {
+        @Override
+        public YamlElement read(Parser parser) {
+            Event event = parser.getNextEvent();
+            if(event.type!=SEQUENCE_START)
+                throw new IllegalStateException();
+            YamlSequence element = new YamlSequence();
+            SequenceStartEvent sequence = (SequenceStartEvent)event;
+            element.setTag(sequence.tag);
+            element.setAnchor(sequence.anchor);
+            readSequenceElements(element);
+            return element;
+        }
+    }
+
+    private class AliasReader implements YamlElementReader {
+        @Override
+        public YamlElement read(Parser parser) {
+            Event event = parser.getNextEvent();
+            if(event.type!=ALIAS)
+                throw new IllegalStateException();
+            YamlAlias element = new YamlAlias();
+            AliasEvent alias = (AliasEvent)event;
+            element.setAnchor(alias.anchor);
+            return element;
+        }
+    }
+
+    private class MappingReader implements YamlElementReader {
+        @Override
+        public YamlElement read(Parser parser) {
+			Event event = parser.getNextEvent();
+			if(event.type!=MAPPING_START)
 				throw new IllegalStateException();
-		}
-	}
-
-	private YamlAlias readAlias() {
-		Event event = parser.getNextEvent();
-		if(event.type!=ALIAS)
-			throw new IllegalStateException();
-		YamlAlias element = new YamlAlias();
-		AliasEvent alias = (AliasEvent)event;
-		element.setAnchor(alias.anchor);
-		return element;
-	}
-
-	private YamlSequence readSequence() {
-		Event event = parser.getNextEvent();
-		if(event.type!=SEQUENCE_START)
-			throw new IllegalStateException();
-		YamlSequence element = new YamlSequence();
-		SequenceStartEvent sequence = (SequenceStartEvent)event;
-		element.setTag(sequence.tag);
-		element.setAnchor(sequence.anchor);
-		readSequenceElements(element);
-		return element;
-	}
+			YamlMapping element = new YamlMapping();
+			MappingStartEvent mapping = (MappingStartEvent)event;
+			element.setTag(mapping.tag);
+			element.setAnchor(mapping.anchor);
+			readMappingElements(element);
+			return element;
+        }
+    }
 
 	private void readSequenceElements(YamlSequence sequence) {
 		while(true) {
@@ -199,17 +232,4 @@ public class YamlDocumentReader {
 			}
 		}
 	}
-
-	private YamlScalar readScalar() {
-		Event event = parser.getNextEvent();
-		if(event.type!= SCALAR)
-			throw new IllegalStateException();
-		ScalarEvent scalar = (ScalarEvent)event;
-		YamlScalar element = new YamlScalar();
-		element.setTag(scalar.tag);
-		element.setAnchor(scalar.anchor);
-		element.setValue(scalar.value);
-		return element;
-	}
-	
 }
